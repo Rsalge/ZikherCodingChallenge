5600d416467a33d360f4508b6e6d247fac2a2878
Ross Salge <rossnsalge@gmail.com>
2018-05-03 13:56:55 -0500

    Removes App component and adds Route for Homepage in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import promise from "redux-promise";
 import reducers from "./reducers";
+import Homepage from "./components/HomePage";
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -13,14 +14,8 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Header />
-        <Switch>
-          <Route path="/games/:id" component={CurrentGame} />
-          <Route path="/previousGames/" component={PreviousGamesList} />
-          {/*Needs to be after /posts/new since it has the wildcard ":id" condition */}
-          <Route path="/newGame" component={NewGame} />
-          <Route path="/" component={Home} />
-        </Switch>
+        <Search />
+        <Route path="/" component={Homepage} />
       </div>
     </BrowserRouter>
   </Provider>,
