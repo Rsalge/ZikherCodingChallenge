098f7dfc1128bbf9dc9cbef1b250101dc0e4ebd8
Ross Salge <rossnsalge@gmail.com>
2018-05-03 15:46:07 -0500

    Modified GetSongsReducer to return song data properly

--- client/src/reducers/GetsSongsReducer.js
+++ client/src/reducers/GetsSongsReducer.js
@@ -3,7 +3,7 @@ import { GET_SONGS } from "../actions";
 export default function(state = [], action) {
   switch (action.type) {
     case GET_SONGS:
-      return action.payload.data;
+      return action.payload.data.tracks.items;
     default:
       return state;
   }
