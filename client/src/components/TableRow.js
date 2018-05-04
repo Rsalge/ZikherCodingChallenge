b3da1371749cf1830b097eb89d338467d50c3e58
Ross Salge <rossnsalge@gmail.com>
2018-05-04 12:23:52 -0500

    Added css to center all fields in the table

--- client/src/components/TableRow.js
+++ client/src/components/TableRow.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const TableRow = () => {
-  return <div>This is a row!</div>;
+const TableRow = props => {
+  console.log(
+    `song fields \nname:${props.song.name}\nartist:${
+      props.song.artists[0].name
+    }\nAlbum: ${props.song.album.name}`
+  );
+  const fields = ["name", "artists[0].name", "album.name"];
+  return (
+    <div className="tableRow">
+      <div>{props.song.name}</div>
+      <div>{props.song.artists[0].name}</div>
+      <div>{props.song.album.name}</div>
+      <img src={props.song.album.images[0].url} />
+    </div>
+  );
 };
 
 export default TableRow;
