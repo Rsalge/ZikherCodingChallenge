a763f7ec1c0114ea2d12ecc74067bf456602061c
Ross Salge <rossnsalge@gmail.com>
2018-05-04 14:10:10 -0500

    Changed TableHeader to stateful componenet

--- client/src/components/TableHeader.js
+++ client/src/components/TableHeader.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { sortSongs } from "../actions";
-const TableHeaders = props => {
-  return (
-    <div className="tableHeaders">
-      {props.headers.map(header => (
-        <div
-          className="tableHeader"
-          onClick={() => props.sortSongs(header.title)}
-        >
-          {header.title}
-        </div>
-      ))}
-    </div>
-  );
-};
+class TableHeaders extends Component {
+  render() {
+    return (
+      <div className="tableHeaders">
+        {this.props.headers.map(header => (
+          <div
+            className="tableHeader"
+            onClick={() => this.props.sortSongs(header.title)}
+          >
+            {header.title}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default connect(null, { sortSongs })(TableHeaders);
