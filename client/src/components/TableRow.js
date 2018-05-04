8060fc1ec9070117e804c759462857be09b29699
Ross Salge <rossnsalge@gmail.com>
2018-05-04 12:53:58 -0500

    Added css styling for SongTable

--- client/src/components/TableRow.js
+++ client/src/components/TableRow.js
@@ -9,10 +9,12 @@ const TableRow = props => {
   const fields = ["name", "artists[0].name", "album.name"];
   return (
     <div className="tableRow">
-      <div>{props.song.name}</div>
-      <div>{props.song.artists[0].name}</div>
-      <div>{props.song.album.name}</div>
-      <img src={props.song.album.images[0].url} />
+      <div className="field">{props.song.name}</div>
+      <div className="field">{props.song.artists[0].name}</div>
+      <div className="field">
+        <div>{props.song.album.name}</div>
+        <img src={props.song.album.images[0].url} />
+      </div>
     </div>
   );
 };
